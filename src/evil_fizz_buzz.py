4803b3e31a7abbfaba803fa3ab96f30dbed98674
romo <robert.moore@zuhlke.com>
2019-02-14 12:53:10 +0000

    outputs comma delimited string of numbers

--- src/evil_fizz_buzz.py
+++ src/evil_fizz_buzz.py
@@ -1,3 +1,10 @@
 class EvilFizzBuzz:
     def compute(self):
-        return "1"
+        comma_delimited_string = "1"
+        for i in range(2, 100):
+            comma_delimited_string += ("," + self.convert(i))
+
+        return comma_delimited_string
+
+    def convert(self, i):
+        return str(i)
