4b79afd6084d15933796314f61ba18896a7b67a6
romo <robert.moore@zuhlke.com>
2019-02-14 12:44:03 +0000

    the first element of the string is 1^

---
+++ src/evil_fizz_buzz.py
@@ -0,0 +1,3 @@
+class EvilFizzBuzz:
+    def compute(self):
+        return "1"
--- test/test.py
+++ test/test.py
@@ -1,7 +1,15 @@
 import unittest
-class test(unittest.TestCase):
-    def test1(self):
-        assert 1 == 1
+
+from src.evil_fizz_buzz import EvilFizzBuzz
+
+
+class EvilFizzBuzzTest(unittest.TestCase):
+    def test_that_the_first_number_is_one(self):
+        evil_fizz_buzz = EvilFizzBuzz()
+        comma_delimited_string = evil_fizz_buzz.compute()
+        self.assertEqual("1", comma_delimited_string[0])
+
+
 
 if __name__ == "__main__":
     unittest.main()
