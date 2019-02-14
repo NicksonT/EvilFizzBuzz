798a02d368405e78cdeca4c1592f3bbaedc37926
Gergely Boldogkoi <gebo@zuhlke.com>
2019-02-14 13:02:00 +0000

    refactored into same test file

--- test/test.py
+++ test/test.py
@@ -14,6 +14,18 @@ class EvilFizzBuzzTest(unittest.TestCase):
         comma_delimited_string = evil_fizz_buzz.compute()
         self.assertEqual("1,2", comma_delimited_string[:3])
 
+    def test_five_turns_into_buzz(self):
+            fizz = EvilFizzBuzz()
+            self.assertEqual("buzz", fizz.convert(5))
+
+    def test_six_does_not_turn_into_buzz(self):
+            fizz = EvilFizzBuzz()
+            self.assertNotEqual("buzz", fizz.convert(6))
+
+    def test_tes_turns_into_buzz(self):
+            fizz = EvilFizzBuzz()
+            self.assertEqual("buzz", fizz.convert(10))
+
 
 if __name__ == "__main__":
     unittest.main()
