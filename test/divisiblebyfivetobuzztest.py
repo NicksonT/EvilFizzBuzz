900c60bdf44a9ef3bcb83f6b6962121e97a91cc1
Gergely Boldogkoi <gebo@zuhlke.com>
2019-02-14 12:58:45 +0000

    divisible by 5 passing Really

--- test/divisiblebyfivetobuzztest.py
+++ test/divisiblebyfivetobuzztest.py
@@ -1,21 +1,21 @@
 import unittest
 
-from evil_fizz_buzz import EvilFizzBuzz
+from src.evil_fizz_buzz import EvilFizzBuzz
 
 
-class Divisible_by_five_to_buzz_test(unittest.TestCase):
+class DivisibleByFiveToBuzzTest(unittest.TestCase):
 
     def test_five_turns_into_buzz(self):
         fizz = EvilFizzBuzz()
-        self.assertEqual("buzz", fizz.compute(5))
+        self.assertEqual("buzz", fizz.convert(5))
 
     def test_six_does_not_turn_into_buzz(self):
         fizz = EvilFizzBuzz()
-        self.assertNotEqual("buzz", fizz.compute(6))
+        self.assertNotEqual("buzz", fizz.convert(6))
 
     def test_tes_turns_into_buzz(self):
         fizz = EvilFizzBuzz()
-        self.assertEqual("buzz", fizz.compute(10))
+        self.assertEqual("buzz", fizz.convert(10))
 
 
 
