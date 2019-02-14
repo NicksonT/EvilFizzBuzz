63d2822f6d58d105e4a1606577b6a40daa5a78b6
Gergely Boldogkoi <gebo@zuhlke.com>
2019-02-14 13:04:00 +0000

    integrated divide by 5

--- test/test.py
+++ test/test.py
@@ -12,25 +12,24 @@ class EvilFizzBuzzTest(unittest.TestCase):
     def test_that_the_second_number_is_two(self):
         evil_fizz_buzz = EvilFizzBuzz()
         comma_delimited_string = evil_fizz_buzz.compute()
-        self.assertEqual("2", comma_delimited_string.split(",")[1])
+        self.assertEqual("1,2", comma_delimited_string[:3])
 
     def test_that_the_third_number_is_Fizz(self):
         evil_fizz_buzz = EvilFizzBuzz()
         comma_delimited_string = evil_fizz_buzz.compute()
         self.assertEqual("Fizz", comma_delimited_string.split(",")[2])
-        self.assertEqual("1,2", comma_delimited_string[:3])
 
     def test_five_turns_into_buzz(self):
-            fizz = EvilFizzBuzz()
-            self.assertEqual("buzz", fizz.convert(5))
+        fizz = EvilFizzBuzz()
+        self.assertEqual("buzz", fizz.convert(5))
 
     def test_six_does_not_turn_into_buzz(self):
-            fizz = EvilFizzBuzz()
-            self.assertNotEqual("buzz", fizz.convert(6))
+        fizz = EvilFizzBuzz()
+        self.assertNotEqual("buzz", fizz.convert(6))
 
-    def test_tes_turns_into_buzz(self):
-            fizz = EvilFizzBuzz()
-            self.assertEqual("buzz", fizz.convert(10))
+    def test_ten_turns_into_buzz(self):
+        fizz = EvilFizzBuzz()
+        self.assertEqual("buzz", fizz.convert(10))
 
 
 if __name__ == "__main__":
